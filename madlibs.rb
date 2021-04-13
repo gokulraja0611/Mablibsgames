822e4accc0aa82cfac6c81353003a34435f6eda4
Gokulraja <82395073+gokulraja0611@users.noreply.github.com>
2021-04-13 14:54:49 +0530

    Add files via upload

---
+++ madlibs.rb
@@ -0,0 +1,33 @@
+puts "Enter the adjective---------"
+adjective =gets.chomp()
+puts("Today i went to the zoo. i saw a "+adjective)
+puts "Enter the noun---------"
+noun =gets.chomp()
+puts(noun+" jumping up and down in its tree")
+puts "Enter the verb and adverb and adjective and noun---------"
+verb =gets.chomp()
+adverb=gets.chomp()
+adjective=gets.chomp()
+noun=gets.chomp()
+puts("He "+verb+" "+adverb+" through the large tunnel that led to its " +adjective+" "+noun)
+puts "Enter the noun---------"
+noun =gets.chomp()
+puts("I got some peanuts and passed them through the cage to a gigantic gray "+noun+" towering above the head")
+puts "Enter the adjective---------"
+adjective =gets.chomp()
+puts("Feeding that animal made me hungry.I went to get a "+adjective+" scoop of ice cream")
+puts "Enter the verb and adverb---------"
+verb =gets.chomp()
+adverb=gets.chomp()
+puts("It filled my stomach. afterwards i had to "+verb+" "+adverb+" to catch our bus")
+puts "Enter the verb and adjective---------"
+verb=gets.chomp()
+adjective =gets.chomp()
+puts("when i got home i "+verb+" my mom for a "+adjective+" day at the zoo.")
+puts("--------------------------------CHECK YOUR ANSWERS------------------------------------------")
+puts("Today I went to the zoo I saw a *naughty *monkey jumping up and down in its tree he
+  *jumped *frequently through large tunnel that led to it's *small *cage.
+  I got some peanuts and passed them through the cage to a gigantic gray *monkey towering above my head .
+  feeding that animal made me hungry.i went to get a *creamy scoop of ice cream.i filled my stomach.
+  Afterwards I had to *run *quickly to catch our bus. when I got home I *thanked mu mom for a *remarkable day at the zoo.
+")
